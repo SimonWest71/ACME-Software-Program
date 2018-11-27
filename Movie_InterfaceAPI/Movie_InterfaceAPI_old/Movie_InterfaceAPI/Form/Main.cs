4d704d2ba1f5d30f46d5418d0783aef4a4a163a9
SimonWest71 <35995280+SimonWest71@users.noreply.github.com>
2018-11-27 13:47:30 +0000

    Add files via upload

---
+++ Movie_InterfaceAPI/Movie_InterfaceAPI_old/Movie_InterfaceAPI/Form/Main.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+using System.Web.Script.Serialization;
+
+namespace Movie_InterfaceAPI
+{
+    public partial class Main : Form
+    {
+        public ImdbEntity_S Entity_S = new ImdbEntity_S();
+        public ImdbEntity_M Entity_M = new ImdbEntity_M();
+        public SeenListUC SeenListUserControl;
+        public WishListUC WishListUserControl;
+
+        public Main()
+        {
+            InitializeComponent();
+
+			//if (Properties.Settings.Default.SeenList == null)
+			//	Properties.Settings.Default.SeenList = new System.Collections.Generic.List<string>();
+			//if (Properties.Settings.Default.WishList == null)
+			//	Properties.Settings.Default.WishList = new System.Collections.Generic.List<string>();
+
+			//Properties.Settings.Default.Save();
+        }
+        private void SearchTb_Enter(object sender, EventArgs e)
+        {
+            if (SearchTb.Text == "Search for movie title...")
+                SearchTb.Text = string.Empty;
+        }
+        private void SearchTb_Leave(object sender, EventArgs e)
+        {
+            if (SearchTb.Text == string.Empty)
+                SearchTb.Text = "Search for movie title...";
+        }
+        public bool TitleInWishList(string title)
+        {
+            if (Properties.Settings.Default.WishList.Contains(title))
+                return true;
+            return false;
+        }
+        public bool TitleInSeenList(string title)
+        {
+            if (Properties.Settings.Default.SeenList.Contains(title))
+                return true;
+            return false;
+        }
+        public int TitlesWished()
+        {
+            return Properties.Settings.Default.WishList.Count;
+        }
+        public void UpdateResultResult()
+        {
+            this.MoviesFLP.Controls.Clear();
+
+            string url = string.Empty;
+
+            if (MultipleCb.Checked)
+            {
+                url = OMDB.multipleAddress + SearchTb.Text.Trim() + "&apikey=" + OMDB.api;
+            }
+            else
+            {
+                url = OMDB.singleAddress + SearchTb.Text.Trim() + "&apikey=" + OMDB.api;
+            }
+
+            using (WebClient wc = new WebClient())
+            {
+                string json = wc.DownloadString(url);
+
+                JavaScriptSerializer oJS = new JavaScriptSerializer();
+
+                if (MultipleCb.Checked)
+                {
+                    Entity_M = oJS.Deserialize<ImdbEntity_M>(json);
+                    var movies = oJS.Serialize(Entity_M);
+
+                    if (Entity_M.Response == "True")
+                    {
+                        foreach (var movie in Entity_M.Search)
+                        {
+                            MovieUC MovieUserControl = new MovieUC(movie.Title, movie.Year, movie.Poster, movie.Type, TitleInWishList(movie.Title), TitleInSeenList(movie.Title), this);
+                            MoviesFLP.Controls.Add(MovieUserControl);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("There was an error requesting movie data", "Error");
+                    }
+                }
+                else
+                {
+                    Entity_S = oJS.Deserialize<ImdbEntity_S>(json);
+
+                    if (Entity_S.Response == "True")
+                    {
+                        MovieUC MovieUserControl = new MovieUC(Entity_S.Title, Entity_S.Year, Entity_S.Poster, Entity_S.Type, TitleInWishList(Entity_S.Title), TitleInSeenList(Entity_S.Title), this);
+                        MoviesFLP.Controls.Add(MovieUserControl);
+                    }
+                    else
+                    {
+                        MessageBox.Show("There was an error requesting movie data", "Error");
+                    }
+                }
+                SeenListUserControl = new SeenListUC(NumberOfMoviesSeenInSearchCategory(SearchTb.Text), this);
+                MoviesFLP.Controls.Add(SeenListUserControl);
+                WishListUserControl = new WishListUC(Properties.Settings.Default.WishList.Count, this);
+                MoviesFLP.Controls.Add(WishListUserControl);
+            }
+        }
+        public int NumberOfMoviesSeenInSearchCategory(string title)
+        {
+            int movieMatched = 0;
+
+            for (int i = 0; i < Properties.Settings.Default.SeenList.Count; i++)
+            {
+                if (Properties.Settings.Default.SeenList[i].Contains(title))
+                {
+                    movieMatched++;
+                }
+            }
+            return movieMatched;
+        }
+        private void SearchBtn_Click(object sender, EventArgs e)
+        {
+            UpdateResultResult();
+        }
+        private void Main_Load(object sender, EventArgs e)
+        {
+            SeenListUserControl = new SeenListUC(NumberOfMoviesSeenInSearchCategory(SearchTb.Text), this);
+            MoviesFLP.Controls.Add(SeenListUserControl);
+            WishListUserControl = new WishListUC(Properties.Settings.Default.WishList.Count, this);
+            MoviesFLP.Controls.Add(WishListUserControl);
+        }
+    }
+}
---
+++ Movie_InterfaceAPI/Movie_InterfaceAPI_old/Movie_InterfaceAPI/UserControl/MovieUC.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Movie_InterfaceAPI
+{
+    public partial class MovieUC : UserControl
+    {
+        private string title;
+        private Main Main;
+
+        public MovieUC(string title, string year, string posterUrl, string desc, bool isInWishList, bool isInSeenList, Main Main)
+        {
+            this.InitializeComponent();
+            this.Main = Main;
+            this.TitleLb.Text = title + " (" + year + ") " + "\n\nType: " + CultureInfo.CurrentCulture.TextInfo.ToTitleCase(desc);
+            this.PosterPb.ImageLocation = posterUrl;
+            this.title = title;
+
+            if (isInWishList)
+            {
+                WishListBtn.Text = "- Wish List";
+            }
+            else
+            {
+                WishListBtn.Text = "+ Wish List";
+            }
+
+            if (isInSeenList)
+            {
+                SeenBtn.Image = Properties.Resources.icons8_eye_23_Copy;
+            }
+            else
+            {
+                SeenBtn.Image = Properties.Resources.icons8_eye_23;
+            }
+        }
+        private void MoreInfoLl_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MovieInfo MovieInfoFrm = new MovieInfo(title);
+            MovieInfoFrm.ShowDialog();
+        }
+        private void WishListBtn_Click(object sender, EventArgs e)
+        {
+            if (!Main.TitleInWishList(title))
+            {
+                Main.Entity_S.AddToWishList(title);
+                MessageBox.Show(this, title + " has been added to the wish list", "Success");
+                WishListBtn.Text = "- Wish List";
+            }
+            else
+            {
+                Main.Entity_S.RemoveFromWishList(title);
+                MessageBox.Show(this, title + " has been removed to the wish list", "Success");
+                WishListBtn.Text = "+ Wish List";
+            }
+            Main.UpdateResultResult();
+        }
+        private void SeenBtn_Click(object sender, EventArgs e)
+        {
+            if (!Main.TitleInSeenList(title))
+            {
+                Main.Entity_S.AddToSeenList(title);
+                MessageBox.Show(this, title + " has been added to the seen list", "Success");
+                SeenBtn.Image = Properties.Resources.icons8_eye_23_Copy;
+            }
+            else
+            {
+                Main.Entity_S.RemoveFromSeenList(title);
+                MessageBox.Show(this, title + " has been removed from the seen list", "Success");
+                SeenBtn.Image = Properties.Resources.icons8_eye_23;
+            }
+            Main.UpdateResultResult();
+        }
+    }
+}
